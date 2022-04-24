fa7b9d6f42a19da46cc439ca7a2e880f6ac687d5
sanglee <pksll325@gmail.com>
2022-04-25 07:30:36 +0900

    docs: add command lines for exp

--- script/run.sh
+++ script/run.sh
@@ -1,9 +1,41 @@
 # baseline train
-python train.py --name WAVLM --extractor WAVLM
-python train.py --name STFT --extractor STFT
-python train.py --name W2VL --extractor W2VL
+python train.py --name WAVLM --extractor WAVLM --arch cnn
+python train.py --name WAVLM --extractor WAVLM --arch resnet18 
+python train.py --name WAVLMP --extractor WAVLM --arch resnet18 -p
+python train.py --name WAVLM --extractor WAVLM --arch lstm
+
+python train.py --name WVLML --extractor WVLML --arch cnn
+python train.py --name WVLML --extractor WVLML --arch resnet18 
+python train.py --name WVLMLP --extractor WVLML --arch resnet18 -p
+python train.py --name WVLML --extractor WVLML --arch lstm
+
+python train.py --name STFT --extractor STFT --arch cnn
+python train.py --name STFT --extractor STFT --arch resnet18
+python train.py --name STFTP --extractor STFT --arch resnet18 -p
+python train.py --name STFT --extractor STFT --arch lstm
+
+python train.py --name W2VL --extractor W2VL --arch cnn
+python train.py --name W2VL --extractor W2VL --arch resnet18
+python train.py --name W2VLP --extractor W2VL --arch resnet18 -p
+python train.py --name W2VL --extractor W2VL --arch lstm
 
 # smote train
-python train.py --smote --name WAVLM-SMOTE --extractor WAVLM
-python train.py --smote --name STFT-SMOTE --extractor STFT
-python train.py --smote --name W2VL-SMOTE --extractor W2VL
+python train.py --smote --name WAVLM-S --extractor WAVLM --arch cnn
+python train.py --smote --name WAVLM-S --extractor WAVLM --arch resnet18
+python train.py --smote --name WAVLMP-S --extractor WAVLM --arch resnet18 -p
+python train.py --smote --name WAVLM-S --extractor WAVLM --arch lstm
+
+python train.py --smote --name WVLML-S --extractor WVLML --arch cnn
+python train.py --smote --name WVLML-S --extractor WVLML --arch resnet18
+python train.py --smote --name WVLMLP-S --extractor WVLML --arch resnet18 -p
+python train.py --smote --name WVLML-S --extractor WVLML --arch lstm
+
+python train.py --smote --name STFT-S --extractor STFT --arch cnn
+python train.py --smote --name STFT-S --extractor STFT --arch resnet18
+python train.py --smote --name STFTP-S --extractor STFT --arch resnet18 -p
+python train.py --smote --name STFT-S --extractor STFT --arch lstm
+
+python train.py --smote --name W2VL-S --extractor W2VL --arch cnn
+python train.py --smote --name W2VL-S --extractor W2VL --arch resnet18
+python train.py --smote --name W2VLP-S --extractor W2VL --arch resnet18 -p
+python train.py --smote --name W2VL-S --extractor W2VL --arch lstm
