3c63cff988d91b501981bc4c6e539dd338ef7a71
sanglee <pksll325@gmail.com>
2022-04-24 09:41:25 +0900

    docs: command line add

---
+++ script/run.sh
@@ -0,0 +1,5 @@
+python train.py --smote --data_path ./feat-WAVLM/ --name WAVLM-SMOTE
+python train.py --smote --data_path ./feat-STFT/ --name STFT-SMOTE
+
+python train.py --data_path ./feat-WAVLM/ --name WAVLM
+python train.py --data_path ./feat-STFT/ --name STFT
